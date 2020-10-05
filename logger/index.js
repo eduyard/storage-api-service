6a39f770b7b60ef3bb4b5efab7e17b5eb02c4603
Anar K. Jafarov <anar.k.jafarov@gmail.com>
2020-10-05 06:48:52 +0300

    removed winston (maybe will return when will see necessity)

--- logger/index.js
+++ logger/index.js
@@ -1,40 +1,12 @@
-const winston = require('winston');
-const ConsoleTransport = winston.transports.Console;
-
-const transports = [];
-const exceptionHandlers = [];
-
-// Transport for logging to console
-const consoleTransport = new ConsoleTransport({
-  level: 'debug',
-  colorize: true,
-  humanReadableUnhandledException: true,
-  timestamp: true
-});
-transports.push(consoleTransport);
-exceptionHandlers.push(consoleTransport);
-
-// Creating logger instance
-const
-  logger = new winston.createLogger({
-    transports,
-    format: winston.format.combine(
-      winston.format.splat(),
-      winston.format.simple()
-    ),
-    exitOnError: false
-  });
-
-// Handling Unhandled Exceptions
-logger.exceptions.handle(exceptionHandlers);
+const logger = console;
 
 // express middleware for request logging
 const morgan = require('morgan');
 logger.request = morgan(
-  'tiny',
+  ':method :url :status :res[content-length] - :response-time ms | :remote-addr ":remote-user"',
   {
     stream: {
-      write: logger.info
+      write: console.info
     }
   }
 );
